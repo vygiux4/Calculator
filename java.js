2757ecaa1be7791454927f4b38b8fdc65f0044bb
vygantas <vygisltu@outlook.com>
2022-05-04 15:05:44 +0300

    operate function and not worrking buttons

--- java.js
+++ java.js
@@ -14,6 +14,21 @@ const calcDivide= function(num1,num2){
 }
 
 
-const operate = function(oper,num1,num2){
+const operate = function(num1,oper,num2){
 
+    if (oper == '+'){
+        return calcAdd(num1,num2)
+    }
+    if (oper == '-'){
+        return calcMinus(num1,num2)
+    }
+    if (oper =='*'){
+        return calcMultiply(num1,num2)
+    }
+    if (oper == '/'){
+        return calcDivide(num1,num2)
+    }
+    
 }
+
+console.log(operate(5,'-',2))
