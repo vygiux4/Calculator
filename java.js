a901a4f662a1baf1f1fe07b6839d7626ca823cfa
vygantas <vygisltu@outlook.com>
2022-05-04 14:28:00 +0300

    basic math operators

--- java.js
+++ java.js
@@ -1,1 +1,19 @@
-console.log('1')
+const calcAdd = function(num1,num2){
+    return num1 + num2
+}
+
+const calcMinus = function(num1,num2){
+    return num1-num2
+}
+const calcMultiply = function(num1,num2){
+    return num1 * num2
+}
+
+const calcDivide= function(num1,num2){
+    return num1 / num2
+}
+
+
+const operate = function(oper,num1,num2){
+
+}
